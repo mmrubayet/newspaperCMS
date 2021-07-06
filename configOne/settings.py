8c2152350360cf0e070522a2dbccf95bc52077f0
mmrubayet <m.rubayet94@gmail.com>
2021-07-06 21:57:55 +0600

    bootstrap styling added

--- configOne/settings.py
+++ configOne/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'crispy_forms',
+
     'accounts',
     'pages',
 ]
@@ -132,3 +135,5 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
